34b6cacdf72c465f43b5e148f6e1c058290880b7
Shafiqmuhammad <115002187+Shafiqmuhammad@users.noreply.github.com>
2023-08-12 01:23:59 +0500

    removed unused code

--- src/app/products/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Products1 } from "@/utils/mock";
-import { ProductCard } from "../productCart";
-import { StaticImageData } from "next/image";
-
-const getProductsByCategory = (category: string) => {
-    return Products1.filter((product => product.category === category));
-};
